f9814f8c0f79400e1185da4d38e456bde18cccd5
A3ToF <mohamedandmostafa77@gmail.com>
2024-07-28 12:48:13 +0300

    Best seller list view item part one

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:booklyapp/Core/utils/assets.dart';
 import 'package:booklyapp/Core/utils/styles.dart';
 import 'package:booklyapp/Features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:booklyapp/Features/home/presentation/views/widgets/featured_list_view.dart';
@@ -20,8 +21,45 @@ class HomeViewBody extends StatelessWidget {
             'Best Seller',
             style: Styles.titleMedium,
           ),
-        )
+        ),
+        SizedBox(
+          height: 22,
+        ),
+        BestSellerListView(),
       ],
+    );
+  }
+}
+
+class BestSellerListView extends StatelessWidget {
+  const BestSellerListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: SizedBox(
+        height: 125,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 2.7 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.red,
+                    image: const DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage(AssetsDate.testIamge),
+                    )),
+              ),
+            ),
+            const Column(
+              children: [Text('')],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
