1758eef14cc9634a753b3be3ff822abf36504191
A3ToF <mohamedandmostafa77@gmail.com>
2024-07-31 13:59:16 +0300

    create api services

---
+++ lib/Core/utils/api_services.dart
@@ -0,0 +1,11 @@
+import 'package:dio/dio.dart';
+
+class ApiServices {
+  final _baseUrl = 'https://www.googleapis.com/books/v1/';
+  final Dio dio;
+  ApiServices(this.dio);
+  Future<Map<String, dynamic>> getBooks({required String endPoint}) async {
+    var response = await dio.get('$_baseUrl$endPoint');
+    return response.data;
+  }
+}
---
+++ lib/Features/home/data/repos/home_repo_implementation.dart
@@ -0,0 +1,18 @@
+import 'package:booklyapp/Core/errors/failures.dart';
+import 'package:booklyapp/Features/home/data/models/book_model/book_model.dart';
+import 'package:booklyapp/Features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImplementation implements HomeRepo {
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+}
